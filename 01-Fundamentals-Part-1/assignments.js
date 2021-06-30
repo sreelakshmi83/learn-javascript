39fa8071be892bc29a24ec4f9b1e48869baf9988
Nirmal Rajendran <nirmal.rajendran@ikea.com>
2021-06-30 10:08:25 +0200

    added fundamentals-01

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,11 @@
+/*Values and Variables 
+1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions) 
+2. Log their values to the console */
+
+let country="sweden";
+let continent="europe";
+let population=10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,53 @@
+////////*linking a file*/////////
+let js="amazing";
+/*if (js="amazing") alert ("js is amazing");*/
+  console.log("great js");
+
+  console.log(2+3+4+5);
+
+ /////////* values and variables*//////////
+
+/*value is a piece of  data it is the most fundamental unit of information
+that we have in programming;*/
+
+console.log("sreelakshmi"); /*sreelakshmi,23 are values*/
+console.log(23);
+
+let firstName="sreelakshmitupakula"; /*firstname is called as a variable so that we can store value(sreelakshmi) in that firstname*/
+                             /* and we can reuse the variable many times in the code*/
+
+
+
+console.log(firstName);     /* it is very useful bcos if i want to change name i just have to change the first name and it displays  new name in all 3 console.log */ 
+console.log(firstName);
+console.log(firstName);
+
+//////////*convention and rules*//////////
+
+/*camel case notation iefirst letter should be in small letter and subsequent word should be in capital letter*/
+/* eg: let 3years=3;*/
+/* variable name cannot start with a number*/
+/* variable should contain only letters ,numbers, underscores, and dollar sign*/
+
+/* eg: let sree&lakshmi="sl";*/
+let sree_lakshmi="sl";
+console.log(sree_lakshmi);
+let $function=90; /*cannot use reserved keywords as a variable name*/
+console.log($function);          /* but can be used with dollarsign*/
+let  person= "sreelakshmi";
+console.log(person);
+let PI = 3.1415 /* we should use uppercase for only constants.bcos constants will never change their value*/
+ console.log(PI);
+/* naming a variable should be easy to understand and we should know what value the variable is holding*/
+
+let myFirstJob="programmer";
+let currentJob="Teacher";
+
+let Job1="programmer";
+let job2="Teacher"; /*  variable names here job1 and job2 are confusing but myFirstJob and currentJob are very easy to understand*/
+
+console.log(myFirstJob);
+
+
+/*in overall a variable is a box and her it is  named as myFirstJob  and we can put any value in it (here it is programmer) and can reuse the variable in the code as many times as we want*/
+
